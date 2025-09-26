1379701e71b58e6b1a9bc53b105361eec4498ae9
uyenluna <pnguye79@asu.edu>
2025-09-25 21:52:32 -0700

    Update cse320lab2_template.v

--- cse320lab2_template.v
+++ cse320lab2_template.v
@@ -225,7 +225,7 @@ module lab2bcd_1digit_tb();
 		.CLR(CLR),
 		.Q(Q),
 		.CO(CO)
-	};
+	);
 
 	// 10ns clock
 	initial CLK = 0;
@@ -265,19 +265,45 @@ endmodule
 //Problem 2b
 //Let D2 and Q2 be defined as the ten's place, or 2nd digit, so for 98, D2 = 9
 module lab2bcd_2digit(D1, D2, ENABLE, LOAD, UP, CLK, CLR, Q1, Q2, CO);
-    input [3:0] ???;
-    input ???;
+    input [3:0] D1, D2;
+    input ENABLE, LOAD, UP, CLK, CLR;
     
-    output [3:0] ???;
-    output reg ???;
+    output [3:0] Q1, Q2;
+    output reg CO;
     
     //Intermediate logic variable(s)
     reg ENABLE_1, ENABLE_2, ENABLE_OVERRIDE, UP_2;
-    ???
+    // Internal wires from digit modules
+    wire [3:0] q1_w, q2_w;
+    wire co1_w, co2_w;
     
     //module instantiations
-    ???
-    
+   lab2bcd_1digit U_DIGIT1 ( // ones digit
+        .D(D1),
+        .ENABLE(ENABLE_1),
+        .LOAD(LOAD),
+        .UP(UP),
+        .CLK(CLK),
+        .CLR(CLR),
+        .Q(q1_w),
+        .CO(co1_w)
+    );
+
+	 lab2bcd_1digit U_DIGIT2 ( // tens digit
+        .D(D2),
+        .ENABLE(ENABLE_2),
+        .LOAD(LOAD),
+        .UP(UP_2),
+        .CLK(CLK),
+        .CLR(CLR),
+        .Q(q2_w),
+        .CO(co2_w)
+    );
+
+	// Drive top-level Qs from internal wires
+    assign Q1 = q1_w;
+    assign Q2 = q2_w;
+
     //Intermediate logic procedural block
     //This is just one way to do it, there's other ways to implement the intermediate logic
     //Cases to consider include:
@@ -286,7 +312,8 @@ module lab2bcd_2digit(D1, D2, ENABLE, LOAD, UP, CLK, CLR, Q1, Q2, CO);
     //Setting the UP for the 2nd digit depending if its going from 0 -> 9 or 9 -> 0 
     always @(*)
     begin
-        if(((Q2 == 4'd0 & Q1 == 4'd0) & UP == 1'd0) || (????????)) begin
+        if(((Q2 == 4'd0 & Q1 == 4'd0) & UP == 1'd0) || 
+		((Q2 == 4'd9 && Q1 == 4'd9) && (UP == 1'd1))) begin
             ENABLE_OVERRIDE = 1'd0;
             CO = 1'd1;
         end		
@@ -295,9 +322,10 @@ module lab2bcd_2digit(D1, D2, ENABLE, LOAD, UP, CLK, CLR, Q1, Q2, CO);
             CO = 1'd0;
 		end
 
-	ENABLE_1 = (ENABLE & ENABLE_OVERRIDE) | (??? & ???); //ENABLE for 1st digit
-	ENABLE_2 = (??? & ENABLE_OVERRIDE) | (ENABLE & ???); //ENABLE for 2nd digit
-	UP_2 = (??? && ???); //UP for 2nd digit
+	ENABLE_1 = (ENABLE & ENABLE_OVERRIDE) | (ENABLE & LOAD); //ENABLE for 1st digit
+	ENABLE_2 = (( (UP && (Q1 == 4'd9)) || (!UP && (Q1 == 4'd0)) ) & ENABLE_OVERRIDE)
+    | (ENABLE & LOAD); //ENABLE for 2nd digit
+	UP_2 = (UP && 1'b1); //UP for 2nd digit
     end
     
 endmodule
@@ -335,6 +363,7 @@ module lab2bcd_1digit_top(D, ENABLE, LOAD, UP, CLK100MHZ, CLR, Q, CO);
     simpleDivider clkdiv(???, CLK, CLR); //Read the simpleDivider module to see what it takes as an input
     lab2bcd_1digit BCD1(???);
 endmodule
+
 
 
 
